8abcdd2dc4a7d2b8ee47db5dc34e51053746a1d0
Luke Green CS2019 <luke.green.2019@uni.strath.ac.uk>
2023-10-15 00:36:07 +0100

    Added dealer functionality and started the players game lord help me

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-#TODO: Add a loop to the gameplay and add the feature of money in
-#TODO: Add insurance and splitting
-#TODO: Add deck checking for multiple cards and discarded cards e.g. 2x Ace of Diamonds (Basically add card counting)
+# TODO: Add a loop to the gameplay and add the feature of money in
+# TODO: Add insurance and splitting
+# TODO: Add deck checking for multiple cards and discarded cards e.g. 2x Ace of Diamonds (Basically add card counting)
 
-#TODO: Maybe add functionality to pick the amount of decks you want
+
+# TODO: Maybe add functionality to pick the amount of decks you want
 
 # https://en.wikipedia.org/wiki/Blackjack
+
+# The Dealer's Play When the dealer has served every player, the dealers face-down card is turned up. If the total is
+# 17 or more, it must stand. If the total is 16 or under, they must take a card. The dealer must continue to take
+# cards until the total is 17 or more, at which point the dealer must stand. If the dealer has an ace, and counting
+# it as 11 would bring the total to 17 or more (but not over 21), the dealer must count the ace as 11 and stand. The
+# dealer's decisions, then, are automatic on all plays, whereas the player always has the option of taking one or
+# more cards.
+
+# That was acutally crazy hard to find took me like 20 mins of googling to find the dealers rules????
 
 import random
 
@@ -17,12 +27,12 @@ dealersCards = []
 gamblersCards = []
 
 testHand = [['Ace', 'Hearts'], [8, 'Clubs'], [5, 'Spades']]
-testHand2 = [['Ace', 'Hearts'], ['King', 'Clubs']]
+testHand2 = [['Ace', 'Hearts'], [8, 'Clubs']]
 
 money = 500
 
 
-# gets a random card from the deck (Doesnt check for duplicates)
+# gets a random card from the deck (Doesn't check for duplicates)
 def getCard():
     Value = number[random.randint(0, len(number) - 1)]
     Suit = suit[random.randint(0, len(suit) - 1)]
@@ -63,33 +73,61 @@ def showHand(hand):  # Shows the hand of the player hate the code tho need to ge
         temp += f"{i[0]} of {i[1]} and the "
     return temp
 
-def dealersTurn(gamblersCards, dealersCards):
-    while getHandValue(gamblersCards) > getHandValue(dealersCards):
 
-        if getHandValue(dealersCards) == 21:
-            return 1
-        elif getHandValue(dealersCards) >= 17:
-            return 2
-        else:
-            gamblersCards.append(getCard())
-    return False
+def dealersTurn(Cards):
+    # If the total is 17 or more, it must stand
+    if getHandValue(Cards) > 17:
+        print(f"The dealer has chosen to stand with the hand {showHand(Cards)}")
+        return "Stand"
+    # If the total is 16 or under, they must take a card.
+    elif getHandValue(Cards) <= 16:
+        print(f"The dealer has hit the hand is now {showHand(Cards)}")
+        Cards.append(getCard())
+        dealersTurn(Cards)  # Cheeky bit of recursion to get me through the day
+    # If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer
+    # must count the ace as 11 and stand.
+    # This is already satisfied by the getHandValue thankfully
+    elif getHandValue(Cards) > 21:
+        return "Bust"
+
+def determineWinner(playerCards, dealerCards):
+    playerValue = getHandValue(playerCards)
+    dealerValue = getHandValue(dealerCards)
+    if playerValue > dealerValue:
+        return "The player wins the hand"
+    elif playerValue < dealerValue:
+        return "The dealer wins the hand"
+    else:
+        return "Standoff money is returned"
 
 
 flag = True
+game = True
 
 while flag:
     initialDeal()
 
     bet = input(f"How much would you like to bet you have £{money} remaining: ")
     print(f"Your cards are {showHand(gamblersCards)}giving you a value of {getHandValue(gamblersCards)}")
+    print(f"The dealer has the revealed {dealersCards[0][0]} of {dealersCards[0][1]}")
 
-    choice = input("Would you like to hit or stand")
-    if choice == "Hit":
-        gamblersCards.append(getCard())
-    elif choice == "Stand":
-        # Do something
-        print('hi')
-    else:
-        print("I dont understand your input try again")
+    # TODO: After 15/10/23 adding functionality and finding the dealers play need to fix game funtionality however too
+    #  tired tonight gonna eat then sleep. Hint for tommorow: Its the stupid showHand function that you hate
+    while game:
+        choice = input("Would you like to hit or stand ")
+        if choice == "Hit":
+            gamblersCards.append(getCard())
+            print(f"Your hand is now {showHand(gamblersCards)}giving you a value of {getHandValue(gamblersCards)}")
+        elif choice == "Stand":
+            print(f"The dealers hand is {showHand(dealersCards)}")
+            dealerOutcome = dealersTurn(dealersCards)
+            if dealerOutcome == "Stand":
+                print(determineWinner(gamblersCards, dealersCards))
+                break
+            elif dealerOutcome == "Bust":
+                print("The dealer has gone bust you win")
+                break
+        else:
+            print("I dont understand your input try again")
 
     flag = False
