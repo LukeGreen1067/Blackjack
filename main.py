6c78b9c0021022f20fb3450286b0396e9e800282
Luke Green CS2019 <luke.green.2019@uni.strath.ac.uk>
2023-10-16 02:13:45 +0100

    Only small commit today got to it late: Fixed bugs from yesterday and finished gameplay loop need to add betting and working out the betting and its done

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ gamblersCards = []
 
 testHand = [['Ace', 'Hearts'], [8, 'Clubs'], [5, 'Spades']]
 testHand2 = [['Ace', 'Hearts'], [8, 'Clubs']]
+testHand3 = [['Ace', 'Hearts'], [8, 'Clubs'], [2, 'Spades'], [5, 'Spades']]
 
 money = 500
 
@@ -70,33 +71,39 @@ def initialDeal():
 def showHand(hand):  # Shows the hand of the player hate the code tho need to get rid of temp
     temp = ""
     for i in hand:
-        temp += f"{i[0]} of {i[1]} and the "
+        if i == hand[-1:][0]:
+            temp += f"{i[0]} of {i[1]} "
+        else:
+            temp += f"{i[0]} of {i[1]} and the "
     return temp
 
 
 def dealersTurn(Cards):
     # If the total is 17 or more, it must stand
-    if getHandValue(Cards) > 17:
+    if getHandValue(Cards) >= 17:
         print(f"The dealer has chosen to stand with the hand {showHand(Cards)}")
         return "Stand"
     # If the total is 16 or under, they must take a card.
     elif getHandValue(Cards) <= 16:
         print(f"The dealer has hit the hand is now {showHand(Cards)}")
         Cards.append(getCard())
-        dealersTurn(Cards)  # Cheeky bit of recursion to get me through the day
+        dealersTurn(Cards)
+        if getHandValue(Cards) > 21:
+            return "Bust"
+        else:
+            return "Stand"
     # If the dealer has an ace, and counting it as 11 would bring the total to 17 or more (but not over 21), the dealer
     # must count the ace as 11 and stand.
     # This is already satisfied by the getHandValue thankfully
-    elif getHandValue(Cards) > 21:
-        return "Bust"
+
 
 def determineWinner(playerCards, dealerCards):
     playerValue = getHandValue(playerCards)
     dealerValue = getHandValue(dealerCards)
     if playerValue > dealerValue:
-        return "The player wins the hand"
+        return f"The player wins the hand with a value of {getHandValue(playerCards)}"
     elif playerValue < dealerValue:
-        return "The dealer wins the hand"
+        return f"The dealer wins the hand with a value of {getHandValue(dealerCards)}"
     else:
         return "Standoff money is returned"
 
@@ -117,7 +124,11 @@ while flag:
         choice = input("Would you like to hit or stand ")
         if choice == "Hit":
             gamblersCards.append(getCard())
-            print(f"Your hand is now {showHand(gamblersCards)}giving you a value of {getHandValue(gamblersCards)}")
+            if getHandValue(gamblersCards) > 21:
+                print("You have gone bust")
+                break
+            else:
+                print(f"Your hand is now {showHand(gamblersCards)}giving you a value of {getHandValue(gamblersCards)}")
         elif choice == "Stand":
             print(f"The dealers hand is {showHand(dealersCards)}")
             dealerOutcome = dealersTurn(dealersCards)
@@ -130,4 +141,16 @@ while flag:
         else:
             print("I dont understand your input try again")
 
-    flag = False
+    play = input("Would you like to play again? Y/N ")
+
+
+    if play.upper() == "Y":
+        dealersCards.clear()
+        gamblersCards.clear()
+        continue
+    elif play.upper() == "N":
+        flag = False
+    else:
+        dealersCards.clear()
+        gamblersCards.clear()
+        continue
